ee73b8a1168cef09a2cd57e8621d98f49e0bca88
zozo404 <enzocuny66@gmail.com>
2022-05-24 16:53:00 +0200

    :construction: | construction  du panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,16 +3,36 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/panier', name: 'app_cart')]
     public function index(Cart $cart): Response
     {
-        return $this->render('cart/index.html.twig');
+        $cartComplete = [];
+
+        foreach ($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                'product'=>$this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity'=>$quantity,
+            ];
+        }
+
+        return $this->render('cart/index.html.twig',[
+            'cart'=>$cartComplete
+        ]);
     }
 
     #[Route('/cart/add/{id}', name: 'add_to_cart')]
@@ -28,4 +48,10 @@ class CartController extends AbstractController
         $cart->remove();
         return $this->redirectToRoute('products');
     }
+     // #[Route('/cart/delete/{id}', name: 'delete_to_cart')]
+    // public function delete(Cart $cart, $id): Response
+    // {
+    //     $cart->delete($id);
+    //     return $this->redirectToRoute('app_cart');
+    // }
 }
